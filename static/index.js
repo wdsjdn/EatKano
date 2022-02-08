5b78214ce6b76ef5f7eb841b69058c592d419550
wdsjdn <99227327+wdsjdn@users.noreply.github.com>
2022-02-08 09:53:04 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,10 +441,10 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
-        if (cps <= 8) return 'TCL';
-        if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
+        if (cps <= 5) return '蔡';
+        if (cps <= 8) return '花生没破防';
+        if (cps <= 10)  return '花生快破防了';
+        if (cps <= 15) return '花生被你整无语了';
         return '人？';
     }
 
